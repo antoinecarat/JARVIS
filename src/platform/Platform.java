76689b9acf2fb8f1e3efd40ef03d04d8f044a29e
Pierre Lemétayer <e127718@etu.univ-nantes.fr>
2017-02-02 18:10:44 +0100

    Add ing JSON parser

--- src/platform/Platform.java
+++ src/platform/Platform.java
@@ -1,7 +1,12 @@
 package platform;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class Platform {
 
@@ -9,7 +14,7 @@ public class Platform {
 		try {
 						
 			//Load config.json
-			String baseClassName = ;
+			String baseClassName = readJSON("config.json");
 			//(new BufferedReader(new FileReader("config.json"))).readLine();
 			
 			//Load base config
@@ -28,6 +33,37 @@ public class Platform {
 		}
 
 		return null;
+	}
+	
+	public static String readJSON(String fileName){
+		try {
+			String json = new String();
+			String line = null;
+			try {
+				BufferedReader reader = new BufferedReader(new FileReader("test.json"));
+				
+			    while ((line = reader.readLine()) != null) {
+			    	json += line;
+			    }
+			    reader.close();
+			    
+				JSONObject obj = new JSONObject(json);
+
+				return obj.getJSONObject("pageInfo").getString("pageName");
+
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+				return null;
+			} catch (IOException e) {
+				e.printStackTrace();
+				return null;
+			}
+			
+		} catch (JSONException e) {
+			e.printStackTrace();
+			return null;
+		}
+		
 	}
 	
 }
