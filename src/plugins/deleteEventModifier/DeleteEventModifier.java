562e42cf70019c7422df72859593d5989f6d91e0
LaurentGirard <laurent.girard44620@gmail.com>
2017-03-30 14:40:17 +0200

    new IModifier added : deleteEventModifier

---
+++ src/plugins/deleteEventModifier/DeleteEventModifier.java
@@ -0,0 +1,18 @@
+package plugins.deleteEventModifier;
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.Map;
+
+import client.IAgenda;
+import client.IEvent;
+import platform.plugins.IModifier;
+
+public class DeleteEventModifier implements IModifier{
+
+	public void modify(IAgenda a, IEvent e, Map<String, Object> list) throws NoSuchFieldException, SecurityException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+					
+		if(!a.getEvents().remove(e)){
+			throw new IllegalArgumentException();
+		}
+	}
+}
